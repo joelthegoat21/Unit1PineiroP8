3f11f393927e26494370bdd32fbd7afbdcd8387c
joelthegoat21 <729037@student.dusd.net>
2025-03-14 15:26:34 -0700

    i made obstacccles for my gAME
    Today i ccan now drive my car  and  can turn it and made dodging obstacles

--- Unit1PlayerControlPineiroP8/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlPineiroP8/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 5.0f;
+   // Private Variables
+   private float speed = 5.0f;
+   private float turnSpeed = 25.0f;
+   private float horizontalInput;
+   private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // We'll move the vehice foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
